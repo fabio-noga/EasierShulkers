caac94578bd076e178f0f18b1790e06fcd813ba4
fabio-noga <fabiomiguel98@hotmail.com>
2022-06-02 01:26:50 +0100

    Easier Shulkers Boxes 1.1!
    // Mod disabled on servers, so it doesn't do weird bugs.

--- src/main/java/com/codesloth/easiershulkers/Events.java
+++ src/main/java/com/codesloth/easiershulkers/Events.java
@@ -1,23 +1,42 @@
 package com.codesloth.easiershulkers;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ShulkerBoxMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Objects;
 
 import static com.codesloth.easiershulkers.Utils.isShulkerBox;
 
 @Mod.EventBusSubscriber(modid = EasierShulkers.MOD_ID)
 public class Events {
 
+    public static boolean IS_SERVER;
+
+    @SubscribeEvent
+    public void onJoinWorld(WorldEvent.Load event) {
+        try{
+            boolean isLan = Objects.requireNonNull(Minecraft.getInstance().getCurrentServer()).isLan();
+            if (IS_SERVER == isLan){
+                IS_SERVER = !isLan;
+            }
+        } catch (Exception ignore){}
+    }
+
     @SubscribeEvent
     public void onPlaceBlock(PlayerInteractEvent.RightClickBlock event){
         Player player = event.getPlayer();
-        if(player == null || player instanceof FakePlayer || player.containerMenu instanceof ShulkerBoxMenu)
+        if(IS_SERVER || player == null || player instanceof FakePlayer || player.containerMenu instanceof ShulkerBoxMenu)
             return;
 
         ItemStack mainHandItem = player.getMainHandItem();
@@ -40,7 +59,7 @@ public class Events {
     @SubscribeEvent
     public void onRightClickItem(PlayerInteractEvent.RightClickItem event) {
         Player player = event.getPlayer();
-        if (player instanceof FakePlayer || player.containerMenu instanceof ShulkerBoxMenu)
+        if (IS_SERVER || player instanceof FakePlayer || player.containerMenu instanceof ShulkerBoxMenu)
             return;
 
         InteractionHand hand = event.getHand();
